5ab39b6d9159598e83aa9385d532a17265e38c46
sagar123singh <s9813558351@gmail.com>
2022-09-10 12:02:16 +0530

    both local and digital watch

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,52 @@
-import React, { useState } from "react";
+/*
+local time
 
-
-
+import React, { useState } from 'react';
 
 const App=()=>{
+ 
+    let newTime= new Date().toLocaleTimeString()
 
+    let [ctime ,setCtime] =useState(newTime);
 
-//const state=useState(5)
-//console.log(state);
-
-const [count,setCount]=useState(0)
-
-const IncNum=()=>{
-    setCount(count + 5)
-//console.log("clicked "+  count++);
-};
-
-
+    const updateTime=()=>{
+        newTime= new Date().toLocaleTimeString();
+        setCtime(newTime);
+    };
 
     return (
         <>
-            <h1>{count}</h1>
-            <button onClick={IncNum}>Click Me</button>
+            <h1> {ctime}</h1>
+            <button onClick={updateTime}> get time</button>
         </>
     );
-};
+}
+export default App;
+*/
+
+
+
+///                   digital clock
+
+import React, { useState } from "react";
+
+const App=()=>{
+    let Time=new Date().toLocaleTimeString();
+
+    const [ctime,setCtime]=useState(Time)
+
+    const updateTime=()=>{
+        Time=new Date().toLocaleTimeString();
+        setCtime(Time)
+    }
+        setInterval(updateTime, 1000);
+    return(
+        <>
+            <h1> Digital Watch</h1>
+        
+            <h2>{ctime}</h2>,
+            
+                </>
+    )
+}
 export default App;
